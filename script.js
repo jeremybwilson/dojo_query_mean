0254745b515e56935d2fadbf03216bd1525eaa27
Jeremy Wilson <jeremybwilson@gmail.com>
2018-10-24 09:47:59 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Assignment - DojoQuery
+// Reviewing JQuery’s code, we see that they use Immediate functions.
+
+// Our assignment is to create our own DojoQuery library that exposes $Dojo to the global namespace. 
+// Let’s go over the functionality that $Dojo should have:
+
+// The ability to click on an HTML element with a specific id.
+// The ability to hover on an HTML element with a specific id.
+// Make sure you wrap your code in an immediate function
+
+const $Dojo = function(elementId){
+    // return immediate function
+    return (function($dojo){
+        // get DOM object with id
+        $dojo.someId = document.getElementById(elementId);
+
+        // use callback when the element is clicked
+        $dojo.click = function (callback){
+            this.someId = addEventListener('click', callback);
+        };
+
+        // use callback function when hovering in or out
+        $dojo.hover = function(hoverin, hoverout){
+            this.someId.addEventListener('mouseover', hoverin);
+            this.someId.addEventListener('mouseout', hoverout);
+        };
+
+        // return the element
+        return $dojo;
+    })({});
+};
